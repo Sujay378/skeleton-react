f389e037f74977965d504752f5d411ae218f276c
Sujay378 <sdas8438@gmail.com>
2024-06-04 22:30:10 +0530

    few changes

--- src/app/network/axiosClient.ts
+++ src/app/network/axiosClient.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export type ResponseType = "json" | "blob";
+type ResponseType = "json" | "blob";
 
-export type GetRequest = {
+type GetRequest = {
   url: string;
   headers: Record<string, string>;
   params: Record<string, string>;
   baseURL: string;
   responseType: ResponseType;
 };
-export type PostRequest<Payload = unknown> = GetRequest & { payload: Payload };
-export type PutRequest = PostRequest;
+type PostRequest<Payload = unknown> = GetRequest & { payload: Payload };
+export type PutRequest<Payload> = PostRequest<Payload>;
 export type DeleteRequest = GetRequest;
 
 const instance = axios.create({
@@ -20,57 +20,29 @@ const instance = axios.create({
   },
 });
 
-const get = <Response = unknown>(props: GetRequest) => {
-  return instance.get<Response>(props.url, {
-    baseURL: props.baseURL,
-    headers: new axios.AxiosHeaders({
-      ...instance.defaults.headers,
-      ...props.headers,
-    }),
-    params: { ...instance.defaults.params, ...props.params },
-    responseType: props.responseType,
-  });
-};
+const getRequestOptions = (props: Omit<GetRequest, "url">) => ({
+  baseURL: props.baseURL,
+  headers: new axios.AxiosHeaders({
+    ...instance.defaults.headers,
+    ...props.headers,
+  }),
+  params: { ...instance.defaults.params, ...props.params },
+  responseType: props.responseType,
+});
+
+const get = <Response = unknown>(props: GetRequest) =>
+  instance.get<Response>(props.url, getRequestOptions(props));
 
 const post = <Response = unknown, Payload = unknown>(
   props: PostRequest<Payload>
-) => {
-  return instance.post<Response>(props.url, props.payload, {
-    baseURL: props.baseURL,
-    headers: new axios.AxiosHeaders({
-      ...instance.defaults.headers,
-      ...props.headers,
-    }),
-    params: { ...instance.defaults.params, ...props.params },
-    responseType: props.responseType,
-  });
-};
+) =>
+  instance.post<Response>(props.url, props.payload, getRequestOptions(props));
 
 const put = <Response = unknown, Payload = unknown>(
-  props: PostRequest<Payload>
-) => {
-  return instance.put<Response>(props.url, props.payload, {
-    baseURL: props.baseURL,
-    headers: new axios.AxiosHeaders({
-      ...instance.defaults.headers,
-      ...props.headers,
-    }),
-    params: { ...instance.defaults.params, ...props.params },
-    responseType: props.responseType,
-  });
-};
+  props: PutRequest<Payload>
+) => instance.put<Response>(props.url, props.payload, getRequestOptions(props));
 
-const deleteReq = <Response = unknown>(props: DeleteRequest) => {
-  return instance.delete<Response>(props.url, {
-    baseURL: props.baseURL,
-    headers: new axios.AxiosHeaders({
-      ...instance.defaults.headers,
-      ...props.headers,
-    }),
-    params: { ...instance.defaults.params, ...props.params },
-    responseType: props.responseType,
-  });
-};
+const deleteReq = <Response = unknown>(props: DeleteRequest) =>
+  instance.delete<Response>(props.url, getRequestOptions(props));
 
-export const api = { get, post, put, delete: deleteReq, instance };
-export default instance;
+export default { get, post, put, delete: deleteReq, instance };
--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -2,4 +2,4 @@ import { format } from "date-fns/format";
 
 //Format will be something like 02 Feb 2024
 export const formatToGB = (date: string | number | Date) =>
-  format(date, "dd MMM yyyy");
+  format(date, "dd MMM, yyyy");
